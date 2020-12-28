33bceacc07397e0cc150a93388ba46928eb686ec
lvyunze <yunze17817462542@outlook.com>
2020-12-28 19:03:00 +0800

    pyalgorithm

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     keywords="algorithms、data structures",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lvyunze/keyword_extract_links",
+    url="https://github.com/lvyunze/pyalgorithm",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
